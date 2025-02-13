17271faf267a332aa260ede7aa63e4305d7b54fd
Alessandro Gramcko <107635555+Agramcko@users.noreply.github.com>
2025-02-13 16:09:31 -0400

    agregue las constantes en relación a las clases del index

---
+++ script.js
@@ -0,0 +1,7 @@
+const contarvalor = document.getElementById("count");
+const colorPart = document.querySelectorAll(".color-part");
+const contenedor = document.querySelector(".contenedor");
+const empezarButtom = document.querySelector("#empezar");
+const resultado = document.querySelector("#resultado");
+const envuelto = document.querySelector(".envuelto");
+
