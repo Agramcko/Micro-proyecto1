75e29411a9899e44651f620b829ecebeb22e4a09
Alessandro Gramcko <107635555+Agramcko@users.noreply.github.com>
2025-02-13 16:42:16 -0400

    referencias iniciales, mapeo de colores y función del juego

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
 const contarvalor = document.getElementById("count");
 const colorPart = document.querySelectorAll(".color-part");
 const contenedor = document.querySelector(".contenedor");
-const empezarButtom = document.querySelector("#empezar");
+const empezarButton = document.querySelector("#empezar");
 const resultado = document.querySelector("#resultado");
 const envuelto = document.querySelector(".envuelto");
+
+
+
+const colores = {
+    verde: {
+        current: "#008000",
+        new: "#11e711",
+    },
+    rojo: {
+        current: "#ff0000",
+        new: "#fd2a2a",
+    },
+    azul: {
+        current: "#0000FF",
+        new: "#2062fc",
+    },
+    amarillo: {
+        current: "#FFFF00",
+        new: "#fafa18",
+    },
+};
+
+let coloresRamdom = []; 
+let pathGeneratorBool = false;
+let count, 
+    clickCount = 0;
+
+
+empezarButton.addEventListener("click", () => {
+    count = 0;
+    clickCount = 0;
+    coloresRamdom = [];
+    pathGeneratorBool = false;
+    pathGenerate();
+} )
+
+
+
 
