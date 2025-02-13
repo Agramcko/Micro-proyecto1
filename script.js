5fc7b98b2d22f9fd19183b7fece8af8f15f42473
Massimo Gramcko <m.gramcko@correo.unimet.edu.ve>
2025-02-13 17:48:54 -0400

    Funciones - JavaScript
    - Función para obtener un valor aleatorio del objeto.
    
    - Función para reproducir la secuencia
    
    -Función para crear retraso para el efecto de parpadeo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const contarvalor = document.getElementById("count");
+const contarValor = document.getElementById("count");
 const colorPart = document.querySelectorAll(".color-part");
 const contenedor = document.querySelector(".contenedor");
 const empezarButton = document.querySelector("#empezar");
@@ -32,6 +32,8 @@ let count,
     clickCount = 0;
 
 
+
+
 empezarButton.addEventListener("click", () => {
     count = 0;
     clickCount = 0;
@@ -43,3 +45,50 @@ empezarButton.addEventListener("click", () => {
 
 
 
+const pathGenerate = () =>  {
+    coloresRamdom.push(generateRandomValor(colores));
+    count = coloresRamdom.length;
+    pathGeneratorBool = true;
+    pathDecide(count);
+};
+
+
+
+
+const generateRandomValor = (obj) => {
+    let arr = Object.keys(obj);
+    return arr[Math.floor(Math.random() * arr.length)];
+};
+
+
+
+const pathDecide = async(count) => {
+    contarValor.innerText = count;
+    for(let i of coloresRamdom){
+        let currentColor = document.querySelector(`.${i}`);
+        await delay(500);
+        currentColor.style.backgroundColor = `${colores[i]["new"]}`;
+        await delay(600);
+        currentColor.style.backgroundColor = `${colores[i]["current"]}`;
+        await delay(600);
+    }
+    pathGeneratorBool = false;
+};
+
+
+
+async function delay(time){
+    return await new Promise((resolve) => {
+        setTimeout(resolve, time);
+    });
+}
+
+
+colorPart.forEach((element) => {
+    element.addEventListener("click", async(e) => {
+        if(pathGeneratorBool){
+            return false;
+        }
+        if(e.target.)
+    })
+})
